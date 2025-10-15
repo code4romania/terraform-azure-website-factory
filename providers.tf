32ce66d10236338224625cda9c8ca52c33abeb22
Andrei Ioniță <hi@andrei.io>
2025-10-15 14:22:06 +0100

    refactor: migrate web_app module to root (#25)

--- providers.tf
+++ providers.tf
@@ -1,12 +1,13 @@
 terraform {
-  cloud {
-    organization = "onghub"
-
-    workspaces {
-      tags = [
-        "website-factory",
-        "azure"
-      ]
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.23"
     }
   }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
